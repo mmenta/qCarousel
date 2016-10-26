b96395d9928894bf55e8b6e9edadbd6cd87dcb03
Menta <xmmenta@WTMZ-TMZ95798.local>
2016-10-26 09:55:57 -0700

    cleanup

--- js/qCarousel.js
+++ js/qCarousel.js
@@ -217,12 +217,6 @@ var qCarousel = {
 
 /*
  *  jQuery OwlCarousel v1.3.3
- *
- *  Copyright (c) 2013 Bartosz Wojciechowski
- *  http://www.owlgraphic.com/owlcarousel/
- *
- *  Licensed under MIT
- *
  */
 
 /*JS Lint helpers: */
